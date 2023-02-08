929b9f1fecb172b5c8a802f1345c15668f9e71cd
zhao <oak7@foxmail.com>
2023-02-08 20:22:11 +0800

    commit

--- src/main/java/algorithm/DuplicateTest.java
+++ src/main/java/algorithm/DuplicateTest.java
@@ -9,7 +9,7 @@ import java.util.BitSet;
  * N 长数组查重
  * 边界：
  * 1. 暂未考虑无法将原始数组读进内存的情况；
- * 无边界读思路：文件逐行读
+ * 无边界读思路：文件逐行读、hash分片
  * 2. 暂未考虑需计算重复元素数量的情况；
  * 元素计数思路：单独存储重复的元素
  * 实现思路：
@@ -17,10 +17,15 @@ import java.util.BitSet;
  * 2. 采取bigMap节省内存占用;
  * 附加题：
  * 1. 有序状态的数组，即只有相邻的元素会重复
+ *
  * @author zz
  */
 public class DuplicateTest {
-
+    /**
+     * 无序查重
+     *
+     * @param array 无序数组
+     */
     public static void shuffleDuplicate(int[] array) {
         BitSet bitSet = new BitSet();
         for (int j : array) {
@@ -32,6 +37,11 @@ public class DuplicateTest {
         }
     }
 
+    /**
+     * 有序查重
+     *
+     * @param array 有序数组
+     */
     public static void orderedDuplicate(int[] array) {
         for (int i = 0; i < array.length; i++) {
             if (i != array.length - 1 && array[i] == array[i + 1]) {
@@ -40,6 +50,13 @@ public class DuplicateTest {
         }
     }
 
+    /**
+     * 初始化数据
+     *
+     * @param width
+     * @param sort
+     * @return
+     */
     public static int[] initDate(int width, boolean sort) {
         int[] arr = new int[width];
         for (int i = 0; i < width; i++) {
